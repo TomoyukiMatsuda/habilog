815ba5960fef755a55925fc4d84df80042e6f442
matsuda.tomoyuki <m.u.p.s.4.15.17@gmail.com>
2020-05-15 08:26:01 +0000

    user sigup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'アカウントを作成しました'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'アカウントの作成に失敗しました'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :age, :profile, :email, :password, :password_confirmation)
+  end
+end
