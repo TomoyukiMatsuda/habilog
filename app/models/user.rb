68840df70b7bb91d4f0fa1b8c66f263af8d3488c
matsuda.tomoyuki <m.u.p.s.4.15.17@gmail.com>
2020-05-15 09:54:26 +0000

    user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :age, length: { maximum: 3 }
+  validates :profile, length: { maximum: 255 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
