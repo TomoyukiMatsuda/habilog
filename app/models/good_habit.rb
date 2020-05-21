2b49c41127ff1ff72ece4789e5ea8d3d8da926d2
matsuda.tomoyuki <m.u.p.s.4.15.17@gmail.com>
2020-05-21 10:07:15 +0000

    トップページをマークアップ

--- app/models/good_habit.rb
+++ app/models/good_habit.rb
@@ -4,18 +4,6 @@ class GoodHabit < ApplicationRecord
   
   validates :name, presence: true, length: { maximum: 100 }
   
-  # 引数日のログが有るかどうかチェックする
-  def log?(date)
-    day = I18n.l date
-    
-    log_days = []
-    self.good_logs.each do |good_log|
-      log_day = I18n.l good_log.created_at
-      log_days.push(log_day)
-    end
-    log_days.include?(day)
-  end
-  
   # コントローラに書いた方が良い？
   # 今日完了済みの習慣を元に戻す(未済み)メソッド
   def unfinished
@@ -25,7 +13,7 @@ class GoodHabit < ApplicationRecord
     good_log.destroy if today == good_log_day
   end
   
-  # 習慣登録日からの何日目かを計算するメソッド
+  # 習慣登録日からの何日目かを計算する
   def days
     day = Date.parse(I18n.l self.created_at) # xxxx-xx-xxに変換して格納
     today = Date.current
